dd3290d63d96327f0cb378a139fddde5cc5be0a6
pero-jolak <74726856+pero-jolak@users.noreply.github.com>
2025-08-17 16:42:33 +0000

    Deploying to gh-pages from @ pero-jolak/pero-jolak.github.io@762f65e59e0001b9717caef1aad51c4c6cd8c3ce 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Here I list the courses I taught.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
