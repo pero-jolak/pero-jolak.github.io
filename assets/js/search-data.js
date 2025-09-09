51b5f5256ffc45314e8bce2369b92035d95e7647
pero-jolak <74726856+pero-jolak@users.noreply.github.com>
2025-09-09 10:47:01 +0000

    Deploying to gh-pages from @ pero-jolak/pero-jolak.github.io@b2131da1f4637896131a68bb9e1d4731f9303501 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "Repositories of the code and data for some of my papers.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
