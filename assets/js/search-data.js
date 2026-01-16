9fc8b3641a22e1cb4c3917e8127862fa3e43b21d
pero-jolak <74726856+pero-jolak@users.noreply.github.com>
2026-01-16 18:36:08 +0000

    Deploying to gh-pages from @ pero-jolak/pero-jolak.github.io@6e3eb5437d3a31bd62c43b17b732e0fd9e8e9034 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/learning/";
           },
-        },{id: "nav-learning",
-          title: "learning",
-          description: "Here, I document my learning, with updates added periodically.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
